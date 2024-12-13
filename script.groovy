808e01e69a192bb709d494c48d232163417c7e17
Sylenia <csaba.farkas1985@gmail.com>
2024-12-13 16:53:25 +0100

    configure git push pom.xml

--- script.groovy
+++ script.groovy
@@ -20,4 +20,24 @@ def deployApp() {
     echo 'deploying the application...'
 }
 
+def pushPomToGit() {
+    echo 'Pushing updated pom.xml to GitHub...'
+    
+    withCredentials([usernamePassword(credentialsId: 'github-credentials', passwordVariable: 'GIT_PASS', usernameVariable: 'GIT_USER')]) {
+
+        // Configure Git user
+        sh """
+            git config user.email "jenkins@example.com"
+            git config user.name "Jenkins CI"
+        """
+        
+        // Add, commit, and push the updated pom.xml
+        sh """
+            git add pom.xml
+            git commit -m "Bump version to $env.IMAGE_NAME"
+            git push https://${GIT_USER}:${GIT_PASS}@github.com/<your-repo-owner>/<your-repo>.git HEAD:branchname
+        """
+    }
+}
+
 return this
